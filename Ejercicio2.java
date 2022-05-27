0fb1143af82619cc252a0321844a4e182391c45b
oscargou <106373116+oscargou@users.noreply.github.com>
2022-05-27 09:55:46 +0200

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		System.out.println("Nombre\tApellido\temail\t\t\tTelefono");
+		System.out.println("------\t--------\t-----\t\t\t--------\n");
+		System.out.println("Luis\tGonzalez\tluis.gonzalez@gmail.com\t600123456");
+			
+	}
+
+}
---
+++ Ejercicio3.java
@@ -0,0 +1,19 @@
+package ejercicios;
+
+public class Ejercicio3 {
+	
+	public static void main(String[] args) {
+		System.out.println("Tecla de escape\tSignificado");
+		System.out.println("---------------\t-------------------------------\n");
+		System.out.println("\\n\t\tSalto de linea");
+		System.out.println("\\t\t\tTabulación");
+		System.out.println("\\\"\t\tPara poner \" (comillas dobles dentro de texto)"
+				+ ", por ejemplo \"Oscar\"");
+		System.out.println("\\\\\t\tEscribir \\ dentro de texto, por ejemplo "
+				+ "\\ejemplo\\");
+		System.out.println("\\'\t\tCuando queremos usar \' (comilla simple), como por "
+				+ "ejemplo \'Dijosorprendido\'");
+	
+	}
+	
+}
